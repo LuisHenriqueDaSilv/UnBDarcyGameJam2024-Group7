048bb3b31fe438ec0319dbdf3dc24956efa48fd6
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2024-11-04 20:58:22 -0300

    First floor

--- run.py
+++ run.py
@@ -1,33 +1,65 @@
 import pygame
 
+from src.Player import Player
+from src.Island import Island
+
+SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 600
+FPS = 60
 
 pygame.init()
-SCREEN_HEIGHT = 600
-SCREEN_WIDTH = 800
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
-
-player = pygame.Rect(250, 350, 100, 100)
-
+pygame.display.set_caption("UnB Darcy Game Jam - Grupo 07")
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
+
+
+allSprites = pygame.sprite.Group()
+floorGroup = pygame.sprite.Group()
+
+player = Player()
+allSprites.add(player)
+
+island = Island('mid', 100, SCREEN_HEIGHT)
+numberOfIslands = SCREEN_WIDTH/island.rect.width
+for i in range (int(numberOfIslands)+1):
+  island = Island('mid', i*island.rect.width, SCREEN_HEIGHT)
+  floorGroup.add(island)
+
 
 running = True
 while running:
 
-  clock.tick(120)
+  clock.tick(FPS) 
   screen.fill((0, 0, 0))
 
-  pygame.draw.rect(screen, (255, 0, 0), player)
+  if not pygame.sprite.spritecollide(player, floorGroup, False):
+    player.ySpeed += 0.2
+    player.falling = True;
+  else: 
+    player.ySpeed = 0;
+    player.falling = False;
+
+  key = pygame.key.get_pressed()
+  if key[pygame.K_a]:
+    player.run(-5)
+  elif key[pygame.K_d]:
+    player.run(5)
+  else:
+    player.changeStatus('idle')
+
+  if key[pygame.K_SPACE]  and not player.falling:
+    print("pulou")
+    player.jump()
+
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
 
-  key = pygame.key.get_pressed()
-  if key[pygame.K_a]:
-    player.move_ip(-1, 0)
-  elif key[pygame.K_d]:
-    player.move_ip(1, 0)
     
-
-  pygame.display.update()
+  allSprites.draw(screen)
+  allSprites.update()
+  floorGroup.draw(screen)
+  floorGroup.update()
+  pygame.display.flip()
 
 pygame.quit()
---
+++ src/Island.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Island(pygame.sprite.Sprite):
+  def __init__(self, type, x, y):
+    self.sprites = {}
+    
+    self.sprites['left'] = pygame.transform.scale(pygame.image.load('assets/Island/left.png'), (100, 100))
+    self.sprites['mid'] = pygame.transform.scale(pygame.image.load('assets/Island/mid.png'), (320/2.5, 160/2.5))
+    self.sprites['right'] = pygame.transform.scale(pygame.image.load('assets/Island/right.png'), (100, 100))
+    
+    self.allSprites = pygame.sprite.Group()
+
+    self.image = self.sprites[type]
+    self.rect = self.image.get_rect()
+    self.rect.topleft = x, y - self.rect.height
+
+    pygame.sprite.Sprite.__init__(self)
+  
